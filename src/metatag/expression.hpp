ff43f40ac49908d37ad0ad92b9f548a361e48d1c
idlecode <idlecode@localhost>
2013-06-01 16:28:59 +0200

    Working on MetatagExpression; Moving to Arch@Len;

--- src/metatag/expression.hpp
+++ src/metatag/expression.hpp
@@ -1,5 +1,5 @@
-#ifndef METATAG_HPP
-#define METATAG_HPP
+#ifndef EXPRESSION_HPP
+#define EXPRESSION_HPP
 
 #include "types.hpp"
 #include <list>
@@ -7,11 +7,11 @@
 namespace mru
 {
 
-class MetatagException : public std::runtime_error {
+class MetatagExpressionException : public std::runtime_error {
 public: 
-  MetatagException(const UnicodeString &a_message, const UnicodeString &a_expression, int a_start, int a_length =0) throw();
-  MetatagException(const UnicodeString &a_message, const UnicodeString &a_expression) throw();
-  ~MetatagException(void) throw();
+  MetatagExpressionException(const UnicodeString &a_message, const UnicodeString &a_expression, int a_start, int a_length =0) throw();
+  MetatagExpressionException(const UnicodeString &a_message, const UnicodeString &a_expression) throw();
+  ~MetatagExpressionException(void) throw();
   std::pair<int, int> range(void) const throw();
   const char* what(void) const throw();
 protected:
@@ -22,40 +22,21 @@ protected:
 
 /* ------------------------------------------------------------------------- */
 
-class Metatag {
-public:
-  typedef Metatag self_type;
-public:
-  Metatag(const UnicodeString &a_name);
-  virtual ~Metatag(void);
-
-  virtual bool initialize(const UnicodeString &a_arguments) = 0;
-
-  virtual UnicodeString operator()(void) = 0;
-  virtual UnicodeString operator()(const UnicodeString &a_text) = 0;
-
-  const UnicodeString& tagName(void) const;
-protected:
-  UnicodeString m_name;
-};
-
-/* ------------------------------------------------------------------------- */
-
 class MetatagExpression {
 public:
   typedef MetatagExpression self_type;
-  static UnicodeString evaluate(const UnicodeString &a_expression, const std::list<Metatag*> &a_bindings);
+  static UnicodeString evaluate(const UnicodeString &a_expression, const std::list<MetatagExpression*> &a_bindings);
 public:
   MetatagExpression(const UnicodeString &a_expression);
   ~MetatagExpression(void);
 
-  void bind(const std::list<Metatag*> &a_bindings);
+  void bind(const std::list<MetatagExpression*> &a_bindings);
   UnicodeString evaluate(void);
-  UnicodeString evaluate(const std::list<Metatag*> &a_bindings);
+  UnicodeString evaluate(const std::list<MetatagExpression*> &a_bindings);
 
 protected:
   UnicodeString m_expression_repr;
-  std::list<Metatag*> m_bindings;
+  std::list<MetatagExpression*> m_bindings;
   struct token {
     int position;
     enum type { text, name, args } type;
@@ -66,7 +47,7 @@ protected:
   };
   data_tree::basic_tree<token> m_parse_tree;
   void build_tree(const UnicodeString &a_expression);
-  Metatag* find_binding(const UnicodeString &a_tagname) const;
+  MetatagExpression* find_binding(const UnicodeString &a_tagname) const;
   UnicodeString execute(const data_tree::basic_tree<token> &a_branch, bool a_prepare =false);
 
   friend data_tree::basic_tree<MetatagExpression::token>* get_inserted_node(data_tree::basic_tree<MetatagExpression::token> *a_branch);
@@ -74,5 +55,5 @@ protected:
 
 } /* namespace mru */
 
-#endif /* METATAG_HPP */
+#endif /* EXPRESSION_HPP */
 
